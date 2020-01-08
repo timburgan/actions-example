7ca0333ac19758b86c994e233de54308aa2caec8
Tim Burgan <timburgan@github.com>
2020-01-08 17:19:55 +1000

    Update stale_issue.rb

--- .github/actions/stale_issue/lib/stale_issue.rb
+++ .github/actions/stale_issue/lib/stale_issue.rb
@@ -4,8 +4,8 @@ puts ''
 puts ''
 puts '=========================='
 puts 'boomstakalaka'
-puts 'having_label: ' + ENV['INPUT_HAVING_LABEL']
-puts 'days_old: ' + ENV['INPUT_DAYS_OLD']
+puts "having_label: #{ENV['INPUT_HAVING_LABEL']}"
+puts "days_old: #{ENV['INPUT_DAYS_OLD']}"
 puts ''
 puts 'other env vars:'
 puts ''
